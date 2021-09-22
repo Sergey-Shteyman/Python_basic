b3dbb0aeff707c2ec2d76d4c8217f7f23232339f
Sergey-Shteyman <84444627+Sergey-Shteyman@users.noreply.github.com>
2021-09-23 00:06:08 +0300

    Задача 6. Монетка 2

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def coin_coordinates(x, y, r):
+    if abs(x) <= r and abs(y) <= r:
+        return "Монетка где-то рядом"
+    else:
+        return "Монетки в области нет"
+
+
+print("Введите координаты монетки: ")
+x = float(input("X: "))
+y = float(input("Y: "))
+r = float(input("Введите радиус: "))
+print("-" * 20)
+print(coin_coordinates(x, y, r))
+
