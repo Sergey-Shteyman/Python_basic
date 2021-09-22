272c8aaa5afe2f7918d58965b748ef416605d8b8
Sergey-Shteyman <84444627+Sergey-Shteyman@users.noreply.github.com>
2021-09-22 23:55:27 +0300

    Задача 5. Наименьший делитель

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def n_d(num):
+    """
+    Находим наименьший делитель числа
+    """
+    lst = []
+    for i in range(2, num + 1):
+        if num % i == 0:
+            lst.append(i)
+    return min(lst)
+
+
+num = int(input("Введите число: "))
+print(f"Наименьший делитель, отличный от единицы: {n_d(num)}")
