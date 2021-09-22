9ace2b0096e31d8076115521ded9f280725e57f2
Sergey-Shteyman <84444627+Sergey-Shteyman@users.noreply.github.com>
2021-09-22 23:47:57 +0300

    Задача 4. Число наоборот 3

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,15 @@
-# TODO здесь писать код
+def reversed_number(num):
+    num = str(num).split('.')
+    reversed_num = [''.join(reversed(i)) for i in num]
+    return float('.'.join(reversed_num))
+
+
+num_1 = reversed_number(float(input("Введите число: ")))
+num_2 = reversed_number(float(input("Введите второе число: ")))
+print()
+print(f"Первое число наоборот: {num_1}",
+      f"Второе число наоборот: {num_2}",
+      f"Сумма: {num_1 + num_2}",
+      sep="\n")
 
 
