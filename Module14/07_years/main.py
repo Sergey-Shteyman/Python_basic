dcd71cb4e203f4db7785eb7672cff01c23d6c28d
Sergey-Shteyman <84444627+Sergey-Shteyman@users.noreply.github.com>
2021-09-23 17:39:13 +0300

    Задача 7. Года

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def danger_jears(jear_1, jear_2):
+    for i in range(jear_1, jear_2 + 1):
+        for j in range(0, 10):
+            if str(i).count(str(j)) == 3:
+                print(i)
+
+
+jear_1 = int(input("Введите первый год: "))
+jear_2 = int(input("Введите второй год: "))
+print('-' * 20)
+print(f"Года от {jear_1} до {jear_2} с тремя одинаковыми цифрами:")
+danger_jears(jear_1, jear_2)
