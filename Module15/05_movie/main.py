486104b04f82347a070cfc80c8f0efae1bf6d5e4
Sergey_Shteyman <Shteiman10@gmail.com>
2022-09-28 17:34:47 +0300

    Sergey Shteyman Module15 #5

--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -1,5 +1,21 @@
 films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
          'Леон', 'Богемская рапсодия', 'Город грехов',
          'Мементо', 'Отступники', 'Деревня']
+likes = []
+def movie_selection():
+    name_film = input("Введите название фильма: ")
+    if likes.__contains__(name_film):
+        print("Вы уже выбрали данный фильм, пожалуйста, выберете другой фильм")
+        movie_selection()
+    elif films.__contains__(name_film):
+        likes.append(name_film)
+    else:
+        print("Такого фильма в списке нет!")
+    answer = input("Хотите продолжить выбор фильма? y/n: ")
+    if answer == "y" or answer == "Y":
+        movie_selection()
+    else:
+        print(films)
+        print(likes)
 
-# TODO здесь писать код
+movie_selection()
