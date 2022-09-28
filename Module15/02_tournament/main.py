64794f08daf83550ef5b5e1337fe340f0c717d9d
Sergey_Shteyman <Shteiman10@gmail.com>
2022-09-28 15:38:38 +0300

    Sergey Shteyman Module15 #2

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,1 +1,4 @@
-# TODO здесь писать код
+crewList = ["Артемий", "Борис", "Влад", "Гоша", "Дима", "Евгений", "Женя", "Захар"]
+for i in range(0, len(crewList)):
+    if i % 2 == 0:
+        print(crewList[i])
