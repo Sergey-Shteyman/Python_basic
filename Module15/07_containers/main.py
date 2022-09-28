3dd60b7320b70af7388382efb741b2e3c62226b2
Sergey_Shteyman <Shteiman10@gmail.com>
2022-09-28 20:12:11 +0300

    Sergey Shteyman Module15 #7

--- Module15/07_containers/main.py
+++ Module15/07_containers/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+quantity_containers = int(input("Введите колличество контейнеров: "))
+list_of_containers = []
+for i in range(1, quantity_containers + 1):
+    print("Введите вес контейнера: ", end='')
+    list_of_containers.append(int(input()))
+new_container = int(input("Введите вес нового контейнера: "))
+for i in range(1, len(list_of_containers) + 1):
+    if list_of_containers[i - 1] < new_container:
+        print("Номер, куда встанет новый контейнер: ", i)
+        break
