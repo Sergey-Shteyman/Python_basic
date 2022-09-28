4d7bbe52192c220858e9d884bcbaa1d42741d528
Sergey_Shteyman <Shteiman10@gmail.com>
2022-09-28 16:44:23 +0300

    Sergey Shteyman Module15 #4

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,13 @@
-# TODO здесь писать код
-
-
+count_of_cards = int(input("Кол-во видеокарт: "))
+list_of_cards = []
+new_list_of_cards = []
+for i in range(1, count_of_cards + 1):
+    print(f"{i} Видеокарта: ", end='')
+    series = int(input())
+    list_of_cards.append(series)
+max_series = max(list_of_cards)
+for i in list_of_cards:
+    if i < max_series:
+        new_list_of_cards.append(i)
+print("\nСтарый список видеокарт:", list_of_cards)
+print("Новый список видеокарт:", new_list_of_cards)
