fa1e1c196f92c6a5d61987d8e095e5f9fc5c4838
Sergey_Shteyman <Shteiman10@gmail.com>
2022-09-28 21:02:49 +0300

    Sergey Shteyman Module15 #8

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+count_list = int(input("Введите колличество элементов списка: "))
+list_numbers = []
+for i in range(1, count_list + 1):
+    print(i, "элемент списка: ", end='')
+    list_numbers.append(int(input()))
+
+shift = int(input("Введите сдвиг: "))
+shift_list = list_numbers[-shift:] + list_numbers[:-shift]
+print("Изначальный список: ", list_numbers, sep='\n')
+print("Сдвинутый список: ", shift_list)
