84395540e048372d144baab16e27818271914635
Sergey_Shteyman <Shteiman10@gmail.com>
2022-09-28 12:50:30 +0300

    Sergey Shteyman Module15 #1

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+n = int(input("Ввудите целое число N: "))
+list = []
+for i in range(1, n + 1):
+    if i % 2 != 0:
+        list.append(i)
+# print(list)
+print(f"Список из нечетных чисел: {list}")
