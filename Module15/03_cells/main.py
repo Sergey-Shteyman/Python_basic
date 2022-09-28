99d41f1af471fccf6eda6dfb467dfdc29a30c484
Sergey_Shteyman <Shteiman10@gmail.com>
2022-09-28 16:26:18 +0300

    Sergey Shteyman Module15 #3

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+listCount = int(input("Кол-во клеток: "))
+effectiveness = []
+inefficient = []
+for i in range(1, listCount + 1):
+    print(f"Эффеективность {i} клетки: ", end='')
+    value = int(input())
+    effectiveness.append(value)
+    if value < i:
+        inefficient.append(value)
+if len(inefficient) != 0:
+    print("Неподходящие значения: ", end='')
+    for i in inefficient:
+        print(i, end=' ')
+else:
+    print("Все клетки подходят!")
