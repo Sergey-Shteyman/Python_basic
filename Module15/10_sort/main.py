41c7fd782b44ae3cf2731c3a3c8f8ef25597ec1f
Sergey_Shteyman <Shteiman10@gmail.com>
2022-09-28 21:40:32 +0300

    Sergey Shteyman Module15 #10

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+len_lst = int(input("Введите колличество элементов списка: "))
+lst = []
+for i in range(1, len_lst + 1):
+    print(i, "элемент списка: ", end="")
+    lst.append(int(input()))
+print(f"Изначальный список: {lst}")
+
+for i in range(len(lst) - 1):
+    for j in range(len(lst) - 1 - i):
+        if lst[j] > lst [j + 1]:
+            lst[j], lst[j + 1] = lst[j + 1], lst[j]
+
+print(f"Отсортированный список: {lst}")
