6f64772f480a475afa9f156bc1243b53b786c112
Sergey_Shteyman <Shteiman10@gmail.com>
2022-09-30 16:52:05 +0300

    Sergey Shteyman Module16 #9

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,8 +1,8 @@
-quantity_skates = int(input("Кол-во коньков: "))
-lst_sizes_skates = []
-for index in range(1, quantity_skates + 1):
+quantity_roller_skates = int(input("Кол-во коньков: "))
+lst_sizes_roller_skates = []
+for index in range(1, quantity_roller_skates + 1):
     print(f"Размер {index}-й пары: ", end='')
-    lst_sizes_skates.append(int(input()))
+    lst_sizes_roller_skates.append(int(input()))
 print("")
 quantity_humans = int(input("Кол-во людей: "))
 lst_sizes_humans = []
@@ -10,7 +10,7 @@ for index in range(1, quantity_humans + 1):
     print(f"Размер ноги {index}-го человека: ", end='')
     lst_sizes_humans.append(int(input()))
 count_humans = 0
-for size in lst_sizes_skates:
+for size in lst_sizes_roller_skates:
     if lst_sizes_humans.__contains__(size):
         count_humans += 1
         lst_sizes_humans.remove(size)
