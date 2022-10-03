f35405036cd4a0f689d347007af1587382606d3f
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-03 15:07:31 +0300

    Sergey Shteyman Module_17 #8

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+import random
+
+quantity_sticks = int(input("Количество палок: "))
+quantity_throws = int(input("Количество бросков: "))
+result = ["I" for _ in range(quantity_sticks)]
+for throw in range(1, quantity_throws + 1):
+    first_shot = random.randint(1, 10)
+    second_shot = random.randint(first_shot, 10)
+    shots = [first_shot, second_shot]
+    for index in range(first_shot - 1, second_shot):
+        result.pop(index)
+        result.insert(index, ".")
+    print(f"Бросок {throw}. Сбиты палки с номера {shots[0]} по номер {shots[1]}")
+result_str = "".join(result)
+print(f"\nРезультат: {result_str}")
+
+
