8df0d4d432de1070e8341393eaf44878936c148c
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-03 10:46:25 +0300

    Sergey Shteyman Module_17 #1

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+text = input("Введите текст: ")
+vowels_letters = "аоиеёэыуюя"
+vowel_chars_text = [char for char in text if char in vowels_letters]
+length = len(vowel_chars_text)
+print(f"\nСписок гласных букв: {vowel_chars_text}", end="\n")
+print(f"Длина списка: {length}")
+
