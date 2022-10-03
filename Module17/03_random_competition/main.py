ae39e6e96d1f28c5f14900fb67a3b04fcef1ca5d
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-03 11:47:46 +0300

    Sergey Shteyman Module_17 #3

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+import random
+
+firs_command = [round(random.uniform(5, 10), 2) for _ in range(20)]
+second_command = [round(random.uniform(5, 10), 2) for _ in range(20)]
+winners = [
+    firs_command[index] if firs_command[index] > second_command[index]
+    else second_command[index] for index in range(20)
+]
+
+print(f"Первая команда: {firs_command}", end='\n')
+print(f"Вторая команда: {second_command}", end='\n')
+print(f"Победители тура: {winners}")
