e7b04a17e750e0fad343df6a51fc0446b6327479
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-04 16:15:34 +0300

    Shteyman Sergey 18 Module #3

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+name_file = input("Название файла: ")
+special_symbols = "@№$%^&\*()"
+extensions = [".txt", ".docx"]
+if name_file[0] in special_symbols:
+    print(f"\nОшибка: название начинается на один из специальных символов. "
+          f"Remove this: {name_file[0]}")
+elif not name_file.endswith(extensions[0]) and not name_file.endswith(extensions[1]):
+    print("\nОшибка: неверное расширение файла. Ожидалось .txt или .docx.")
+else:
+    print("\nФайл назван верно.")
