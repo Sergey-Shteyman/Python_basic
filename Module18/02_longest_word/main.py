449d60449e1e4f5b1cfc2d91b86de157808d552b
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-04 15:52:50 +0300

    Shteyman Sergey 18 Module #2

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+user_input = input("Введите строку: ").split(" ")
+longest_word = max(user_input, key=len)
+len_of_longest_word = len(longest_word)
+print(f"Самое длинное слово: {longest_word}"
+      f"\nДлина этого слова: {len_of_longest_word}")
