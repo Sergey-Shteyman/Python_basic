d900b429f343d8580a5f7ba7ac1fcc141bbf1a6e
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-04 20:03:34 +0300

    Shteyman Sergey 18 Module #7

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -3,18 +3,17 @@ def compress(str_txt):
     result = ''
 
     if str_len > 0:
-        i = 0
-        while i < str_len:
+        index = 0
+        while index < str_len:
             counter = 0
-            curr_char = str_txt[i:i + 1]
-            while i < str_len and str_txt[i] == curr_char:
-                i += 1
+            curr_char = str_txt[index:index + 1]
+            while index < str_len and str_txt[index] == curr_char:
+                index += 1
                 counter += 1
             result += curr_char + str(counter)
     return result
 
 
 text = input('Введите строку: ')
-
 print(compress(text))
 
