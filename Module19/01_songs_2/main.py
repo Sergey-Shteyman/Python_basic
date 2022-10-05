469c4a16354f5abf821d59b3cf4e1bb722117ba1
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-05 17:29:57 +0300

    Shteyman Sergey 19 Module #1

--- Module19/01_songs_2/main.py
+++ Module19/01_songs_2/main.py
@@ -9,5 +9,16 @@ violator_songs = {
     'Blue Dress': 4.29,
     'Clean': 5.83
 }
+enumerations = [
+    "первой", "второй", "третей", "четвертой",
+    "пятой", "шестой", "седьмой", "восьмой", "девятой"
+]
+total_time = 0
+user_input = int(input("Сколько песен выбрать? "))
+user_songs = [input(f"Название {enumerations[index]} песни: ") for index in range(user_input)]
+for index in range(len(user_songs)):
+    if user_songs[index] in violator_songs:
+        total_time += violator_songs[user_songs[index]]
+print(f"Общее время звучания песен: {round(total_time, 2)}")
 
-# TODO здесь писать код
+
