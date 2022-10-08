324a18fc1ca2434b87e1c5375b44f450638be655
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-08 10:46:09 +0300

    Shteyman Sergey 19 Module #8

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+maximum_number = int(input("Введите максимальное число: "))
+all_numbers = set(range(1, maximum_number + 1))
+
+while True:
+    user_input = input("Нужное число есть среди вот этих чисел: ").lower()
+    if user_input == 'помогите!':
+        break
+    user_input = {int(num) for num in user_input.split()}
+    answer = input("Ответ Артема: ").lower()
+    if answer == 'да':
+        all_numbers &= user_input
+    else:
+        all_numbers -= user_input
+
+print("Артём мог загадать следующие числа:", *all_numbers)
