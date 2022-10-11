41b7f95154d680b43e1a816ab1319f6473ff82dd
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-11 16:07:04 +0300

    Shteyman Sergey 20 Module #6

--- Module20/06_pairs/main.py
+++ Module20/06_pairs/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+first_solution = [
+    (numbers[index], numbers[index + 1]) for index in range(0, len(numbers) - 1, 2)
+]
+second_solution = list(zip(numbers[::2], numbers[1::2]))
+print(first_solution)
+print(second_solution)
