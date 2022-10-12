49d856153dc448329ea6186bc02a9efab34a954a
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-12 11:14:08 +0300

    Shteyman Sergey 20 Module #10 Refactored

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,12 +1,10 @@
 def my_zip(chars, numbers):
-    return ((chars[index], numbers[index]) for index in range(len(min(chars, numbers))))
+    return ((chars[index], numbers[index]) for index in range(min(len(chars), len(numbers))))
 
 
 chars = list(input("Строка: "))
-print(chars)
 numbers = input("Кортеж чисел (через пробел): ").split(" ")
 result = my_zip(chars, numbers)
 print("\nРезультат:", result, sep='\n')
 for item in result:
     print(item)
-
