b791e8c4b8bad15a780bd46ca8d1908f3d9e621e
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-13 09:21:22 +0300

    Shteyman Sergey 21 Module #1

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def recursive_output(number):
+    if number < 1:
+        return 1
+    recursive_output(number - 1)
+    print(number)
+    return number
+
+
+user_input = int(input("Введите число: "))
+recursive_output(user_input)
