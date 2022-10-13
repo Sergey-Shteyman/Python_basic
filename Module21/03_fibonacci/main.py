0ec3045ca163789d63f65a1b3cd068aa81a0b369
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-13 09:24:15 +0300

    Shteyman Sergey 21 Module #3

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def fibonacci(number):
+    if number in (1, 2):
+        return 1
+    result = fibonacci(number - 1) + fibonacci(number - 2)
+    return result
+
+
+user_input = int(input("Введите позицию числа в ряде Фибоначчи: "))
+number_fibonacci = fibonacci(user_input)
+print(f"Число {number_fibonacci}")
