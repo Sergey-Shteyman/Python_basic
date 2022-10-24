e00be3139e23913d3184767d5e40d58df7a835ae
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-24 20:56:31 +0300

    Shteyman Sergey 22 Module #9

--- Module22/09_war_and_peace/main.py
+++ Module22/09_war_and_peace/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+import os
+import zipfile
+
+#Не понимаю почему не сработало..(
+# archive = os.path.abspath("voyna-i-mir.zip")
+# directory_to_extract_to = os.path.abspath("09_war_and_peace")
+# # with zipfile.ZipFile(archive, 'r') as zip_file:
+# #     zip_file.extractall(directory_to_extract_to)
+# extracted_arhive = zipfile.ZipFile(archive)
+# extracted_arhive.extractall(directory_to_extract_to)
+# extracted_arhive.close()
+
+str_open = open(os.path.abspath("voyna-i-mir.txt"), 'r', encoding='utf-8')
+list_file = str_open.read()
+count_dict = {}
+count_letter = 0
+for letter in list_file:
+    if letter.isalpha():
+        x = count_dict.get(letter, 0)
+        count_dict[letter] = x + 1
+        count_letter += 1
+count_letter_dict = [(k, "{:8.6f}".format(count_dict[k] / count_letter)) for k in count_dict.keys()]
+str_open.close()
+count_letter_dict.sort(key=lambda x: x[1], reverse=True)
+print()
+for i in count_letter_dict:
+    print(i[0] + " " + i[1])
