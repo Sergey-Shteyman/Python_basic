4557b06c0e63a4354a0b14b50b9e25c9edca4c66
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-25 15:39:21 +0300

    Shteyman Sergey 23 Module #1

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+summ = 0
+line_count = 0
+name_file = "people.txt"
+error_not_found_file = f"ОШИБКА: файл {name_file} не обнаружен"
+error_length_name = 'ОШИБКА: длина {} строки меньше трех символов в строке'
+
+try:
+    with open(name_file, "r", encoding='utf-8') as peopl:
+        names = peopl.read().split('\n')
+        for person in names:
+            line_count += 1
+            summ += len(person)
+            try:
+                if len(person) < 3:
+                    raise BaseException
+            except BaseException:
+                print(error_length_name.format(line_count))
+                with open('errors.log', 'w') as errors:
+                    errors.write(error_length_name.format(line_count))
+        print(f"Общее количество символов: {summ}")
+
+except FileNotFoundError:
+    print(error_not_found_file)
+    with open('errors.log', 'w') as errors:
+        errors.write(f"ОШИБКА: файл {name_file} не обнаружен")
