06732aa3366ddafaa74c0040342e63187d2353de
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-31 14:08:24 +0300

    Shteyman Sergey 26 Module #4

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def hoftshadter_sequence(start, limit):
+    if start != [1, 1]:
+        return
+    seq = start[:]
+    counter = 1
+    while counter <= limit:
+        q = seq[-seq[-1]] + seq[-seq[-2]]
+        seq.append(q)
+        counter += 1
+        yield q
+
+
+for num in hoftshadter_sequence([1, 1], 33):
+    print(num, end=' ')
