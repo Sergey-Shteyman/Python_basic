588905620f6f69860943e97ba0d040bfad37b4d0
Sergey_Shteyman <Shteiman10@gmail.com>
2022-10-31 11:15:20 +0300

    Shteyman Sergey 25 Module #2 Refactored code. Add raise for errors. Handle errors in new func. Removed wrong line breaks describe the error. Rename func "life". Now it has a more informative name.

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,22 +1,16 @@
 import karma
 
 
-def life():
+def try_to_live_life_and_achieve_nirvana():
     print("----Добро пожаловать в игру под названием жизнь!----")
     current_life_points = 0
-    errors_file_name = "karma.log"
     while current_life_points < karma.Karma().get_karma():
         response = karma.Karma().one_day()
         if isinstance(response, int):
             current_life_points += response
-        else:
-            print(response.__str__())
-            with open(errors_file_name, "a") as errors:
-                errors.write(response.__str__())
-                break
         print(f"Набрано очков жизни: {current_life_points}")
     if current_life_points >= 500:
         print("Поздравляем, вы успешно прожили жизнь и достигли нирваны!)")
 
 
-life()
+try_to_live_life_and_achieve_nirvana()
