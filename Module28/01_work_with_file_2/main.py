c31b86b088ea067a7bfeb0f9147dd1d5769b3c70
Sergey_Shteyman <Shteiman10@gmail.com>
2022-11-02 17:54:04 +0300

    Shteyman Sergey 28 Module #1

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,35 @@
-# TODO здесь писать код
+class File:
+
+    def __init__(self, name: str, mode: str, encoding=None):
+        self.name = name
+        self.mode = mode
+        self.encoding = encoding
+        self.file = None
+
+    def __enter__(self):
+        try:
+            self.file = open(self.name, self.mode, encoding=self.encoding)
+            return self.file
+        except FileNotFoundError:
+            print(FileNotFoundError)
+            self.file = open(self.name, "w", encoding=self.encoding)
+            return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+        if exc_type:
+            print(exc_type)
+        else:
+            print(f"Файл закрылся: {self.file.closed}")
+
+
+# with File("text.txt", "r") as file:
+#     data = file.write("Hello World")
+#     print(data)
+
+with File("text.txt", "a") as file:
+    file.write("Hello World")
+
+with File("text.txt", "r") as file:
+    data = file.read()
+    print(data)
