701502cae7e542ec6bc2838e13c23031912309f9
Sergey_Shteyman <Shteiman10@gmail.com>
2022-11-04 09:56:45 +0300

    Shteyman Sergey 29 Module #5

--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -9,3 +9,15 @@ def singleton(cls):
 
     return wrapped_function
 
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
