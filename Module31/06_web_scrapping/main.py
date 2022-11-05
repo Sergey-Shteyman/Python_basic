15a1f8ceed1bec5d42f9fba1d75ec6c0ee6037f8
Sergey_Shteyman <Shteiman10@gmail.com>
2022-11-05 20:46:15 +0300

    Shteyman Sergey 31 Module #6

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.columbia.edu/~fdc/sample.html'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+items = [h3.text for h3 in soup.findAll('h3')]
+print(items)
